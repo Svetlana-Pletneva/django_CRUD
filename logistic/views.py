4a6684c522fee0b7ad194ef417f569ca00dc6f96
SV-Pletneva <bg026@yandex.ru>
2022-08-25 23:50:06 +0500

    first commit

---
+++ logistic/views.py
@@ -0,0 +1,28 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.viewsets import ModelViewSet
+
+from logistic.models import Product, Stock
+from logistic.serializers import ProductSerializer, StockSerializer
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = ['title']
+    search_fields = ['title', 'description']
+    # ordering_fields = ['id', 'title', 'stock', 'price']
+    pagination_class = PageNumberPagination
+
+
+class StockViewSet(ModelViewSet):
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = ['products']
+    search_fields = ['products__id', 'products__title', 'products__description']
+    # ordering_fields = ['id', 'address']
+    pagination_class = PageNumberPagination
+
